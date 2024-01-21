27bee785edddb4f92f225ee39d1a1eadc4a943b3
TanTGU <tranthanhtantg9630@gmail.com>
2024-01-21 20:47:40 +0700

    Add project files.

---
+++ WebBanSach/Controllers/CategroryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBanSach.Data;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class CategroryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategroryController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategory =_db.Categories.ToList();
+            return View(objCategory);
+        }
+    }
+}
---
+++ WebBanSach/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanSach.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
